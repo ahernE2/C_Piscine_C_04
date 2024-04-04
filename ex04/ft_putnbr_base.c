ce72939b28e65237b83375e94a61cfc19008444f
Alejandro Hernandez Hernandez <alejhern@cbr12s3.42barcelona.com>
2024-04-04 14:00:51 +0200

    ex04 no pasa test

---
+++ ex04/ft_putnbr_base.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 11:03:29 by alejhern          #+#    #+#             */
+/*   Updated: 2024/04/04 13:59:11 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	check_base(char *base, int base_len)
+{
+	int	pos;
+	int	pos_tmp;
+
+	pos = 0;
+	if (base_len < 2)
+		return (0);
+	while (base[pos])
+	{
+		pos_tmp = pos + 1;
+		if (base[pos] == '+' || base[pos] == '-')
+			return (0);
+		while (base [pos_tmp])
+		{
+			if (base[pos] == base[pos_tmp])
+				return (0);
+			pos_tmp++;
+		}
+		pos++;
+	}
+	return (1);
+}
+
+void	ft_print_putnbr_base(int nbr, char *base, int base_len)
+{
+	if (nbr < 0)
+	{
+		if (nbr < -2147483647)
+			ft_print_putnbr_base(-2147483647, base, base_len);
+		else
+		{
+			ft_putchar('-');
+			nbr = -nbr;
+		}
+	}
+	if (nbr > base_len)
+		ft_print_putnbr_base(nbr / base_len, base, base_len);
+	ft_putchar(base[nbr % base_len]);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int	base_len;
+
+	base_len = ft_strlen(base);
+	if (check_base(base, base_len))
+		ft_print_putnbr_base(nbr, base, base_len);
+}
